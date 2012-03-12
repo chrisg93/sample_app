9a220bd9824348fbdaf48a6dd116add40e1343d1
Christophe Gasnier <chrisg93@hotmail.com>
2012-03-12 17:39:31 +0100

    Added user following

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -9,31 +9,41 @@ describe PagesController do
 
   describe "GET 'home'" do
     it "returns http success" do
-      get 'home'
+      get :home
       response.should be_success
     end
     
     it "should have the right title" do
-      get 'home'
+      get :home
       response.should have_selector("title", :content => @base_title + " | Home")
     end
     
     describe "when signed in" do
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        other_user = Factory(:user, :email => Factory.next(:email))
+        other_user.follow!(@user)
+      end
+    
       it "should paginate users" do
-        user = Factory(:user)
         (1..99).each do |i|
-          Factory(:micropost, :user => user, :content => "Baz quux")
+          Factory(:micropost, :user => @user, :content => "Baz quux")
         end
-        test_sign_in user
-        get 'home'
+        get :home
         response.should have_selector("div.pagination")
         response.should have_selector("span.disabled", :content => "Previous")
         response.should have_selector("a", :href => "/?page=2", :content => "2")
         response.should have_selector("a", :href => "/?page=2", :content => "Next")
       end
+
+      it "should have the right follower/following counts" do
+        get :home
+        response.should have_selector("a", :href => following_user_path(@user), :content => "0 following")
+        response.should have_selector("a", :href => followers_user_path(@user), :content => "1 follower")
+      end
     end
   end
-
+  
   describe "GET 'contact'" do
     it "returns http success" do
       get 'contact'
